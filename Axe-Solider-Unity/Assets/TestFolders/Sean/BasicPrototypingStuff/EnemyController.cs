10dbd99b5ec3daf7138648021b6d7c1bf538369c
Sean Mercer <60895318+NotSean725@users.noreply.github.com>
2020-07-27 20:55:44 -0500

    Worked on a basic enemy

---
+++ Axe-Solider-Unity/Assets/TestFolders/Sean/BasicPrototypingStuff/EnemyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed = 3.5f;
+    Rigidbody rig;
+
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 pos = Vector3.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
+        rig.MovePosition(pos);
+        transform.LookAt(target);
+    }
+}
