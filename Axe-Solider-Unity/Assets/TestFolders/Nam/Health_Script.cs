9e399106ff0236b51d68659efb52b8577fe31bff
Namtaskic <nnnamman3@gmail.com>
2020-08-28 00:08:11 -0500

    Health System
    I started the health system. The basics of it.

---
+++ Axe-Solider-Unity/Assets/TestFolders/Nam/Health_Script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health_Script : MonoBehaviour
+{
+    private int health;
+    private int healthMax;
+
+    public Health_Script(int healthMax)
+    {
+        this.healthMax = healthMax;
+        health = healthMax;
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public void Damage(int damageValue)
+    {
+        health -= damageValue;
+        if (health < 0) health = 0;
+    }
+    public void Heal(int healValue)
+    {
+        health += healValue;
+        if (health > healthMax) health = healthMax;
+    }
+}
