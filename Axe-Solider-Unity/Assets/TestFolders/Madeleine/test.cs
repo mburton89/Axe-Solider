9f722d213c510734707c2040114cd3f96729f9ad
WEBSTER UNIVERSITY <DTS@WEBSTER.EDU>
2020-08-31 11:13:33 -0500

    worked on animations

---
+++ Axe-Solider-Unity/Assets/TestFolders/Madeleine/test.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyTest2 : MonoBehaviour
+{
+    public float deathDistance = 0.5f;
+    public float distanceAway;
+    //public Transform thisObject;
+    public Transform target;
+    private NavMeshAgent navComponent;
+    public float dist;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        navComponent = this.gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+
+        dist = Vector3.Distance(target.position, transform.position);
+
+        if (dist <= 30)
+        {
+            navComponent.SetDestination(target.position);
+
+            if (dist >= 30)
+            {
+                target = null;
+            }
+        }
+
+        else
+        {
+            if (target = null)
+            {
+                target = this.gameObject.GetComponent<Transform>();
+            }
+            else
+            {
+                target = GameObject.FindGameObjectWithTag("Player").transform;
+            }
+        }
+    }
+}
