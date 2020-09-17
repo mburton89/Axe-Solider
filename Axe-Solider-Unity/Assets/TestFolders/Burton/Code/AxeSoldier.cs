8049863c4863cbab76f51f2787ea4f58350d8fcd
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2020-09-17 16:52:00 -0500

    Added jump, idle, and run animations to Player Character

---
+++ Axe-Solider-Unity/Assets/TestFolders/Burton/Code/AxeSoldier.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeSoldier : MonoBehaviour
+{
+    public float projectileSpeed;
+    public float fireRate;
+
+    [SerializeField]
+    private Transform _thrownAxeSpawnPoint;
+    [SerializeField]
+    private ThrownAxe _thrownAxePrefab;
+    [SerializeField]
+    private KeyCode throwKey;
+
+    private bool _canThrowAxe;
+
+    private void Start()
+    {
+        _canThrowAxe = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(throwKey) && _canThrowAxe)
+        {
+            ThrowAxe();
+        }
+    }
+
+    void ThrowAxe()
+    {
+        ThrownAxe thrownAxe = Instantiate(_thrownAxePrefab, _thrownAxeSpawnPoint.position, _thrownAxeSpawnPoint.rotation);
+        thrownAxe.GetComponent<Rigidbody>().AddForce(transform.forward * projectileSpeed);
+        StartCoroutine(FireRateBuffer());
+    }
+
+    private IEnumerator FireRateBuffer()
+    {
+        _canThrowAxe = false;
+        yield return new WaitForSeconds(fireRate);
+        _canThrowAxe = true;
+    }
+}
---
+++ Axe-Solider-Unity/Assets/TestFolders/Burton/Code/ThrownAxe.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrownAxe : MonoBehaviour
+{
+    public float rotationSpeed;
+    private Vector3 _rotation;
+
+    private void Start()
+    {
+        _rotation = new Vector3(0,0,1);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(_rotation * (rotationSpeed * Time.deltaTime));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
