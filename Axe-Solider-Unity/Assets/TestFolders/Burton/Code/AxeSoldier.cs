246882aee6c212a926aa6e256a8a9c90705a1eef
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2020-10-04 21:25:53 -0500

    Tied in axe throw animation

--- Axe-Solider-Unity/Assets/TestFolders/Burton/Code/AxeSoldier.cs
+++ Axe-Solider-Unity/Assets/TestFolders/Burton/Code/AxeSoldier.cs
@@ -4,57 +4,26 @@ using UnityEngine;
 
 public class AxeSoldier : MonoBehaviour
 {
-    public float projectileSpeed;
-    public float fireRate;
-
-    [SerializeField]
-    private Transform _thrownAxeSpawnPoint;
-    [SerializeField]
-    private ThrownAxe _thrownAxe;
-    [SerializeField]
-    private Transform _pivot;
-    [SerializeField]
-    private KeyCode throwKey;
-    [SerializeField]
-    private Animator _animator;
-
-    private bool _canThrowAxe;
+    [SerializeField] private Transform _pivot;
+    [SerializeField] private KeyCode throwKey;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private AxeThrowManager _axeThrowManager;
+    [HideInInspector] public bool canThrowAxe;
 
     private void Start()
     {
-        _canThrowAxe = true;
+        canThrowAxe = true;
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && _canThrowAxe)
+        if (Input.GetMouseButtonDown(0) && canThrowAxe)
         {
-            ThrowAxe();
+            _axeThrowManager.ThrowAxe();
         }
         else if (Input.GetMouseButtonUp(0))
         {
-            RetrieveAxe();
+            _axeThrowManager.RetrieveAxe();
         }
-    }
-
-    void ThrowAxe()
-    {
-        _thrownAxe.isThrown = true;
-    }
-
-    void RetrieveAxe()
-    {
-        _thrownAxe.isThrown = false;
-    }
-
-    private IEnumerator ThrowAxeCo()
-    {
-        _canThrowAxe = false;
-        _animator.SetBool("isAttacking", true);
-        yield return new WaitForSeconds(.5f);
-        ThrowAxe();
-        yield return new WaitForSeconds(5f);
-        _canThrowAxe = true;
-        _animator.SetBool("isAttacking", false);
     }
 }
---
+++ Axe-Solider-Unity/Assets/TestFolders/Burton/Code/AxeThrowManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeThrowManager : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _initialSprite;
+    [SerializeField] private SpriteRenderer _axeThrowSprite;
+    [SerializeField] private List<Sprite> _axeThrowSprites;
+    [SerializeField] private float _secondsBetweenFrames;
+    [SerializeField] private ThrownAxe _axe;
+
+    public void ThrowAxe()
+    {
+        StartCoroutine(ThrowAxeCo());
+    }
+
+    private IEnumerator ThrowAxeCo()
+    {
+        _initialSprite.enabled = false;
+        _axeThrowSprite.enabled = true;
+
+        for (int i = 0; i < _axeThrowSprites.Count; i++)
+        {
+            if (i == _axeThrowSprites.Count - 1)
+            {
+                _axe.isThrown = true;
+            }
+
+            _axeThrowSprite.sprite = _axeThrowSprites[i];
+            yield return new WaitForSeconds(_secondsBetweenFrames);
+        }
+
+        _initialSprite.enabled = true;
+        _axeThrowSprite.enabled = false;
+    }
+
+    public void RetrieveAxe()
+    {
+        _axe.isThrown = false;
+    }
+}
