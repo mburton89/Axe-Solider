c1b3131e5e88f5bb6059cdfbb164319bae36ec08
WEBSTER UNIVERSITY <DTS@WEBSTER.EDU>
2020-10-23 13:03:52 -0500

    Added code

---
+++ Axe-Solider-Unity/Assets/TestFolders/Sean/Code/DeathScreen.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DeathScreen : MonoBehaviour
+{
+    public static DeathScreen Instance;
+    public GameObject Container;
+    public Button ReloadButton;
+    public Button QuitButton;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void OnEnable()
+    {
+        ReloadButton.onClick.AddListener(Reload);
+        QuitButton.onClick.AddListener(QuitGame);
+    }
+
+    private void OnDisable()
+    {
+        ReloadButton.onClick.RemoveListener(Reload);
+        QuitButton.onClick.RemoveListener(QuitGame);
+    }
+
+    public void Activate()
+    {
+        Container.SetActive(true);
+    }
+
+    public void Deactivate()
+    {
+        Container.SetActive(false);
+    }
+
+    void Reload()
+    {
+        AxeSoldier.Instance.RespawnPlayer();
+        Deactivate();
+    }
+
+    void QuitGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Axe-Solider-Unity/Assets/TestFolders/Sean/Code/Respawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    // Make empty GameObject in scene then drag it to this name. This will be where Axe Soldier respawns.
+    public Transform respawnPoint;
+
+    private void Update()
+    {
+        // Checks to see if Axe Soldier's health is at 0 or below
+        /*if ()
+        {
+            RespawnPlayer();
+        }*/
+    }
+
+    private void RespawnPlayer()
+    {
+        transform.position = respawnPoint.position;
+    }
+
+}
