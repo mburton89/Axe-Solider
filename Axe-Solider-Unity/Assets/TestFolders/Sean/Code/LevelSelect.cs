86bd6e7ac681a0e7a4122702773a07364c3a4bbb
Sean Mercer <60895318+NotSean725@users.noreply.github.com>
2020-11-02 17:20:54 -0600

    added collider to axe soldier, added SlowlyHurtPlayer to lava, placed in portal, made basic upgrade menu

---
+++ Axe-Solider-Unity/Assets/TestFolders/Sean/Code/LevelSelect.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+}
