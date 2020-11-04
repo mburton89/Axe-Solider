56fed70c62f8ebbe55d1a58eefd619535a86e6bd
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2020-11-04 11:58:41 -0600

    Added spotlightmover in case we want it

---
+++ Axe-Solider-Unity/Assets/Global/Code/SpotLightMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SpotLightMover : MonoBehaviour
+{
+    public Vector3 startRotation;
+    public Vector3 endRotation;
+    public float duration;
+    public Ease ease;
+
+    private void Start()
+    {
+        StartCoroutine(SwivelCo());
+    }
+
+    private IEnumerator SwivelCo()
+    {
+        transform.DORotate(endRotation, duration, RotateMode.Fast);
+        yield return new WaitForSeconds(duration);
+        transform.DORotate(startRotation, duration, RotateMode.Fast);
+        yield return new WaitForSeconds(duration);
+        StartCoroutine(SwivelCo());
+    }
+}
