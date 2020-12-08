af02d881322b8118970d2984b18df3bb916c3685
Sean Mercer <60895318+NotSean725@users.noreply.github.com>
2020-12-08 12:05:36 -0600

    Added in opening and closing cutscenes and did some touch-ups

---
+++ Axe-Solider-Unity/Assets/TestFolders/Sean/Code/LastTeleportPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LastTeleportPlayer : MonoBehaviour
+{
+    public GameObject VideoPlayer;
+    public float setTime;
+    public GameObject HUD;
+
+    private void Start()
+    {
+        VideoPlayer.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            HUD.SetActive(false);
+            VideoPlayer.SetActive(true);
+            Destroy(VideoPlayer, setTime);
+
+            StartCoroutine(LateCall());
+        }
+    }
+
+    IEnumerator LateCall()
+    {
+        yield return new WaitForSeconds(setTime);
+        HUD.SetActive(true);
+
+        SceneManager.LoadScene(1);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
